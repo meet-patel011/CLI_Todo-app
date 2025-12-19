20fcf09360b08b52ded5fbee8861232bc73a2925
Meet Patel <pmeet0989@gmail.com>
2025-12-19 10:51:15 +0530

    Update cli_Todo.js

--- cli_Todo.js
+++ cli_Todo.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 
 const command = process.argv[2];
-const argument = process.argv[3];
+const argument1 = process.argv[3];
+const argument2 = process.argv[4];
 
 function loadTasks() {
   const data = fs.readFileSync("tasks.json", "utf-8");
@@ -16,21 +17,69 @@ function addTask(task) {
   const tasks = loadTasks();
   tasks.push(task);
   saveTasks(tasks);
-  console.log("Task added!");
+  console.log("Task added");
 }
 
 function listTasks() {
   const tasks = loadTasks();
-  console.log("Your Tasks:");
-  for (let i = 0; i < tasks.length; i++) {
-    console.log((i + 1) + ". " + tasks[i]);
+
+  if (tasks.length === 0) {
+    console.log("No tasks found");
+    return;
   }
+
+  console.log("Your Tasks:");
+  tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task}`);
+  });
+}
+
+function deleteTask(index) {
+  const tasks = loadTasks();
+
+  if (index < 1 || index > tasks.length) {
+    console.log("Invalid task number");
+    return;
+  }
+
+  tasks.splice(index - 1, 1);
+  saveTasks(tasks);
+  console.log("Task deleted");
+}
+
+function updateTask(index, newTask) {
+  const tasks = loadTasks();
+
+  if (index < 1 || index > tasks.length) {
+    console.log("Invalid task number");
+    return;
+  }
+
+  tasks[index - 1] = newTask;
+  saveTasks(tasks);
+  console.log("Task updated");
 }
 
 if (command === "add") {
-  addTask(argument);
+  addTask(argument1);
+
 } else if (command === "list") {
   listTasks();
+
+} else if (command === "delete") {
+  deleteTask(parseInt(argument1));
+
+} else if (command === "update") {
+  updateTask(parseInt(argument1), argument2);
+
 } else {
-  console.log("Command not found");
+  console.log(`
+Unknown command
+
+Available commands:
+node todo.js add "Task name"
+node todo.js list
+node todo.js delete <taskNumber>
+node todo.js update <taskNumber> "New task name"
+`);
 }
